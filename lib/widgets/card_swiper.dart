26bfdff72cfaffdbd65c5f7ef570d2ea34cbff85
yderfre78 <72670647+yderfre78@users.noreply.github.com>
2021-08-21 18:39:42 -0500

    construccion slider swiper

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -5,10 +6,21 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
       width: double.infinity,
-      height: 500.0,
-      color: Colors.red,
+      height: size.height * 0.5,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.9,
+        itemBuilder: (_, int index) {
+          return FadeInImage(
+              placeholder: NetworkImage('https://via.placeholder.com/300x400'),
+              image: NetworkImage('https://via.placeholder.com/300x400'));
+        },
+      ),
     );
   }
 }
